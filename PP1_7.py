fb3997826196146b8cd467bce367b2ee8429cc27
Karcihan-Satheskishan-ICD2O1-01 <karcihan.satheskishan@student.tdsb.on.ca>
2024-09-26 13:22:59 +0000

    l;

--- PP1_7.py
+++ PP1_7.py
@@ -9,7 +9,7 @@ def q1():
   print(bool1)
 
 def q2():
-  num=input(int("Input an integer: "))
+  num=int(input("Input an integer: "))
   bool1=(num > 5)
   print(bool1)
 
@@ -24,15 +24,17 @@ def q4():
   print(bool1)
 
 def q5():
-  word1=input(int("Input an integer: "))
-  word2=input(int("Input another integer: "))
-
+  num1=int(input("Input an integer: "))
+  num2=int(input("Input another integer: "))
+  bool1=(num1*num2 > 40)
+  print(f"Your numbers multiplied together are greater than 40: {bool1}")
 
 #Do edit the code below
 #Comment the lines below when running your tests
-
+'''
 q1()
 q2()
 q3()
 q4()
 q5()
+'''
